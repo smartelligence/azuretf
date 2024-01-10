0911053d7c271ece37b70bdec47208de38f65462
Andrew Smart <andrew@smartelligence.onmicrosoft.com>
2024-01-10 19:56:42 +0100

    update

--- testvm.tf
+++ testvm.tf
@@ -10,7 +10,7 @@ resource "azurerm_linux_virtual_machine" "example" {
 
   admin_ssh_key {
     username   = "adminuser"
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = file("ssh/id_rsa.pub")
   }
 
   os_disk {
